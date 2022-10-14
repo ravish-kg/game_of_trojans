46c637c3038a57284655bd9eff39b178fce6d4c2
ravishkg <kalapana@usc.edu>
2022-10-14 12:53:35 -0700

    Timer animation code added

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -10,7 +10,7 @@ public class CountdownTimer : MonoBehaviour
     public float startTime1;
     public GameObject hollowNumber;
     private SpriteRenderer c;
-    [SerializeField] Text countdownText1;
+    // [SerializeField] Text countdownText1;
     public GameObject mazeChangeText;
 
     void Start() {
@@ -19,38 +19,46 @@ public class CountdownTimer : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if(countdownText1 != null) {
-            currentTime1 -= 1 * Time.deltaTime;
-            countdownText1.text = currentTime1.ToString("0");
-
-            if (mazeChangeText)
+        if (mazeChangeText)
+        {
+            if (Timer.currentTime.ToString("0").Equals("20"))
             {
-                if (Timer.currentTime.ToString("0").Equals("20"))
-                {
-                    currentTime1 = currentTime1 + (5 * Time.deltaTime);
-                }
+                currentTime1 = currentTime1 + (5 * Time.deltaTime);
             }
+        }
+
+        // if(countdownText1 != null) {
+        //     currentTime1 -= 1 * Time.deltaTime;
+        //     countdownText1.text = currentTime1.ToString("0");
+
+        //     if (mazeChangeText)
+        //     {
+        //         if (Timer.currentTime.ToString("0").Equals("20"))
+        //         {
+        //             currentTime1 = currentTime1 + (5 * Time.deltaTime);
+        //         }
+        //     }
             
 
-            // Debug.Log("current time="+countdownText.text);
-            if(currentTime1 <= 0) {
-                currentTime1 = 0;  
-                countdownText1.text = ""; 
-                Destroy(countdownText1.gameObject); 
-                Destroy(gameObject);
-            }
-        }
-        if(Collision.count == 3) {
-            Destroy(countdownText1.gameObject);
-            Destroy(gameObject);
-        }
+        //     // Debug.Log("current time="+countdownText.text);
+        //     if(currentTime1 <= 0) {
+        //         currentTime1 = 0;  
+        //         countdownText1.text = ""; 
+        //         Destroy(countdownText1.gameObject); 
+        //         Destroy(gameObject);
+        //     }
+        // }
+        // if(Collision.count == 3) {
+        //     Destroy(countdownText1.gameObject);
+        //     Destroy(gameObject);
+        // }
     }
 
     private void OnTriggerEnter2D(Collider2D col) {
-        if(col.tag == "Player") {
-            currentTime1 = 0;
-            countdownText1.text = ""; 
-            Destroy(countdownText1.gameObject);  
-        }
+        // if(col.tag == "Player") {
+        //     currentTime1 = 0;
+        //     countdownText1.text = ""; 
+        //     Destroy(countdownText1.gameObject);  
+        // }
     }
 }
