1a2dfbb458d5b619c77f697ca53e3e7741f761a9
ravishkg <kalapana@usc.edu>
2022-11-18 16:27:16 -0800

    added changes

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -65,10 +65,12 @@ public class GameOver : MonoBehaviour
                     carryOverFlag = false;
                 }
 
-                // Leaderboard score logic - STARTS
+                 // Leaderboard score logic - STARTS
                 if(!isScoreUpdated) {
                     // Need to change to take name as input... Should change savescore logic
-                    scoreManager.AddScore(new Score("rav", 3, level));
+                    float score = (float)System.Math.Round(Timer1.startingTime - Timer1.cTime, 2);
+                    string name = PlayerPrefs.GetString("playerName");
+                    scoreManager.AddScore(new Score(name, score, level));                    
                     scoreManager.SaveScore();
                     isScoreUpdated = true;
                 }
