46b9433cd8d2363137ad336f3e021d45cf2a239c
Tejaswini2103 <tummu@usc.edu>
2022-11-26 13:56:22 -0800

    added set2 game over condition when all blocks are destroyed

---
+++ Assets/Scripts/Level3Set2GameEnd.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3Set2GameEnd : MonoBehaviour
+{
+    public GameObject block1;
+    public GameObject block2;
+    public GameObject block3;
+    public GameObject block4;
+    public GameObject block5;
+    public GameObject block6;
+    public GameObject popupCanvas;
+    public GameObject TimerCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (block1 == null && block2 == null && block3 == null && block4 == null && block5 == null && block6 == null && !popupCanvas.active && TimerCanvas == null)
+        {
+            Timer3.currentTime = 0;
+        }
+
+    }
+}
+
+
